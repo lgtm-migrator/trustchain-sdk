86797cc8d3f38ff8e2937066c748b459e4554b70
Mirko Mollik <mollik@trustcerts.de>
2022-06-10 14:00:57 +0200

    fix: print error in ci
    Signed-off-by: Mirko Mollik <mollik@trustcerts.de>

--- packages/vc-jwt/src/lib/vc-create-jwt.test.ts
+++ packages/vc-jwt/src/lib/vc-create-jwt.test.ts
@@ -50,6 +50,7 @@ describe('vc', () => {
    * @returns A JWT-encoded verifiable credential with example data
    */
   async function createVc(): Promise<string> {
+    if (!config.config.invite) throw Error();
     const vcIssuerService = new VerifiableCredentialIssuerService();
 
     return await vcIssuerService.createVerifiableCredential(
@@ -58,7 +59,7 @@ describe('vc', () => {
         type: ['TestCredential'],
         credentialSubject: { id: 'did:max:mustermann' },
         id: 'unique_id',
-        issuer: config.config.invite!.id,
+        issuer: config.config.invite.id,
         // nonce: 'randomVC',
       },
       cryptoServiceRSA
