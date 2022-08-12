4d45319ec8bbb6264c809fd32dea1e80aced2f6e
Mirko Mollik <mollik@trustcerts.de>
2022-08-12 13:25:43 +0200

    fix: remove logging
    Signed-off-by: Mirko Mollik <mollik@trustcerts.de>

--- packages/did/src/lib/did.ts
+++ packages/did/src/lib/did.ts
@@ -129,7 +129,6 @@ export abstract class Did {
    * @param structure structure of the element
    */
   protected parseTransactionControllers(structure: DidStructure) {
-    console.log(structure);
     if (structure.controller?.remove) {
       structure.controller.remove.forEach((id) =>
         this.controller.current.delete(id)
